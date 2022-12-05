076ea2b07c6ebd83d20211e7a040a91dfb7e016b
Kai Dyer <kaidyer29@gmail.com>
2022-12-05 15:05:58 -0500

    Initial Commit

---
+++ MDRDarts/MDRDartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDRDartsApp.swift
+//  MDRDarts
+//
+//  Created by Kai Dyer on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MDRDartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
